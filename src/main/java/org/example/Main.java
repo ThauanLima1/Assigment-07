4fa6d430c5a155d0f1092cbb49ef267d0134e11c
ThauanLima1 <mitoduxmito@gmail.com>
2025-11-05 23:19:03 -0300

    Add main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,13 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        PersonFactory factory = new PersonFactory();
+        Person person1 = factory.createPerson("Thauan", 40);
+        Person person2 = factory.createPerson("Sponge", 25);
+        Person person3 = factory.createPerson("Bob", 30);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
     }
 }
